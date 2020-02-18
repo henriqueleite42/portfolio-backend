8175322f8bc371190c815b3a473dcdf3cb9ee992
Henrique Leite <henriqueleite616@gmail.com>
2020-02-17 21:49:48 -0300

    Fixed Cors Requests

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Set CORS headers
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.FRONT_URL);
+  next();
+});
+
 require('./src/router')(app);
 
 app.listen(process.env.PORT, () => console.log('Server Started'));
